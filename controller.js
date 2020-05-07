ff438478f59a16b48bae59a6f42c68bf3c800a5a
Agustina Elisabeth Siahaan <elis170899@outlook.com>
2020-05-07 23:21:30 +0700

    put data untuk mengedit data di tabel t_sparepart

--- controller.js
+++ controller.js
@@ -155,3 +155,20 @@ exports.ubahmontir = function(req,res){
             }
         });
 };
+
+//mengubah data sparepart berdasarkan id
+exports.ubahsparepart = function(req,res){
+    var id = req.body.id_sparepart;
+    var nama_sparepart = req.body.nama_sparepart;
+    var harga_sparepart = req.body.harga_sparepart;
+    var satuan = req.body.satuan;
+
+    connection.query('UPDATE t_sparepart SET nama_sparepart=?, harga_sparepart=?, satuan=? WHERE id_sparepart=? ', [nama_sparepart, harga_sparepart, satuan, id],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.ok("Berhasil Ubah Data Sparepart", res)
+            }
+        });
+};
